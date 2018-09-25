9e18133a1868ba6572ffcaac2bbf774002328f41
XiaoYan <liujingyan@congmingpai.com>
2018-09-25 10:34:34 +0800

    括号着色,显示文件大小

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
 VS Code 扩展插件使用:
+
+1.Bracket Pair Colorizer:对括号进行着色;
+2.file-size:右下角显示文件大小;
--- text.js
+++ text.js
@@ -2,8 +2,12 @@ function add(a,b)
 {
    if (a > b) {
     console.log(a);
+    console.error(a);
        
    }else{
        console.log(b);
-   }
+       console.error(b);
+    }
 }
+
+add(1,2);
